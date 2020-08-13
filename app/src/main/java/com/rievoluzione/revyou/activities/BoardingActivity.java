626f624a857b188cc64bf5bfc196458e2688ade6
tamaasrory <tamaasrory@gmail.com>
2020-08-13 10:08:13 +0700

    fix BoardingActivity

--- app/src/main/java/com/rievoluzione/revyou/activities/BoardingActivity.java
+++ app/src/main/java/com/rievoluzione/revyou/activities/BoardingActivity.java
@@ -26,105 +26,19 @@ import com.rievoluzione.revyou.R;
 
 public class BoardingActivity extends AppCompatActivity {
 
-    ViewPager.PageTransformer pageTransformer = new ViewPager.PageTransformer() {
-        private static final float MIN_SCALE = 0.85f;
-        private static final float MIN_ALPHA = 0.5f;
-
-        @Override
-        public void transformPage(@NonNull View view, float position) {
-            int pageWidth = view.getWidth();
-            int pageHeight = view.getHeight();
-
-            if (position < -1) { // [-Infinity,-1)
-                // This page is way off-screen to the left.
-                view.setAlpha(0f);
-
-            } else if (position <= 1) { // [-1,1]
-                // Modify the default slide transition to shrink the page as well
-                float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
-                float vertMargin = pageHeight * (1 - scaleFactor) / 2;
-                float horzMargin = pageWidth * (1 - scaleFactor) / 2;
-                if (position < 0) {
-                    view.setTranslationX(horzMargin - vertMargin / 2);
-                } else {
-                    view.setTranslationX(-horzMargin + vertMargin / 2);
-                }
-
-                // Scale the page down (between MIN_SCALE and 1)
-                view.setScaleX(scaleFactor);
-                view.setScaleY(scaleFactor);
-
-                // Fade the page relative to its size.
-                view.setAlpha(MIN_ALPHA +
-                        (scaleFactor - MIN_SCALE) /
-                                (1 - MIN_SCALE) * (1 - MIN_ALPHA));
-
-            } else { // (1,+Infinity]
-                // This page is way off-screen to the right.
-                view.setAlpha(0f);
-            }
-
-        }
-    };
     private ViewPager viewPager;
     private MyViewPagerAdapter myViewPagerAdapter;
     private RelativeLayout dotsLayout;
     private TextView[] dots;
-
-    //  viewpager change listener
-    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
-        public int moveTo = 0;
-        @Override
-        public void onPageSelected(int position) {
-        }
-
-        @Override
-        public void onPageScrolled(int arg0, float arg1, int arg2) {
-            if (arg1 > 0) {
-                int tmp1 = 0;
-                int tmp2 = 0;
-                moveTo = (moveTo == 0) && (arg2 > 1000) ? 2 : 1;
-
-                if (moveTo == 1) {
-                    tmp1 = (int) (25 * arg1);
-                    tmp2 = -1 * (int) (25 * (arg1));
-                } else {
-                    tmp1 = -1 * (int) (25 * arg1);
-                    tmp2 = (int) (25 * (arg1));
-                }
-
-                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-                        RelativeLayout.LayoutParams.WRAP_CONTENT,
-                        RelativeLayout.LayoutParams.WRAP_CONTENT
-                );
-
-                int margin = convertDpToPixel(arg0 * 25, getApplicationContext());
-
-                params.setMargins(margin, 0, 0, 0);
-                params.addRule(RelativeLayout.CENTER_VERTICAL);
-                dots[0].setLayoutParams(params);
-                if (moveTo == 1) {
-                    dots[0].setTranslationX(convertDpToPixel(tmp1, getApplicationContext()));
-                    dots[arg0 + 1].setTranslationX(convertDpToPixel(tmp2, getApplicationContext()));
-                } else {
-                    dots[arg0 + 1].setTranslationX(convertDpToPixel(tmp1, getApplicationContext()));
-                    dots[0].setTranslationX(convertDpToPixel(tmp2, getApplicationContext()));
-                }
-            }
-        }
-
-        @Override
-        public void onPageScrollStateChanged(int arg0) {
-            Log.d("onPageScrollState", "onPageScrollStateChanged: " + arg0);
-            if (arg0 == 0) {
-                moveTo = 0;
-                Log.d("onPageScrolledtTOO 2", "after selected ---> " + moveTo);
-            }
-        }
-    };
     private int[] layouts;
     private Button btnNext;
-    private Session session;
+
+    /* uncomment this if you need launch boarding at first time only */
+    // private Session session;
+
+    //  viewpager change listener
+    private ViewPager.OnPageChangeListener viewPagerPageChangeListener;
+    private ViewPager.PageTransformer pageTransformer;
 
     /**
      * This method converts dp unit to equivalent pixels, depending on device density.
@@ -135,17 +49,6 @@ public class BoardingActivity extends AppCompatActivity {
      */
     public static int convertDpToPixel(int dp, Context context) {
         return dp * ((int) context.getResources().getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT);
-    }
-
-    /**
-     * This method converts device specific pixels to density independent pixels.
-     *
-     * @param px      A value in px (pixels) unit. Which we need to convert into db
-     * @param context Context to get resources and device specific display metrics
-     * @return A float value to represent dp equivalent to px value
-     */
-    public static int convertPixelsToDp(int px, Context context) {
-        return px / ((int) context.getResources().getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT);
     }
 
     @Override
@@ -183,11 +86,102 @@ public class BoardingActivity extends AppCompatActivity {
         layouts = new int[]{
                 R.layout.welcome_slide1,
                 R.layout.welcome_slide2,
-                R.layout.welcome_slide2
+                R.layout.welcome_slide3
         };
 
         dotSetup();
         changeStatusBarColor();
+
+        pageTransformer = new ViewPager.PageTransformer() {
+            private static final float MIN_SCALE = 0.85f;
+            private static final float MIN_ALPHA = 0.5f;
+
+            @Override
+            public void transformPage(@NonNull View view, float position) {
+                int pageWidth = view.getWidth();
+                int pageHeight = view.getHeight();
+
+                if (position < -1) { // [-Infinity,-1)
+                    // This page is way off-screen to the left.
+                    view.setAlpha(0f);
+
+                } else if (position <= 1) { // [-1,1]
+                    // Modify the default slide transition to shrink the page as well
+                    float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
+                    float vertMargin = pageHeight * (1 - scaleFactor) / 2;
+                    float horzMargin = pageWidth * (1 - scaleFactor) / 2;
+                    if (position < 0) {
+                        view.setTranslationX(horzMargin - vertMargin / 2);
+                    } else {
+                        view.setTranslationX(-horzMargin + vertMargin / 2);
+                    }
+
+                    // Scale the page down (between MIN_SCALE and 1)
+                    view.setScaleX(scaleFactor);
+                    view.setScaleY(scaleFactor);
+
+                    // Fade the page relative to its size.
+                    view.setAlpha(MIN_ALPHA +
+                            (scaleFactor - MIN_SCALE) /
+                                    (1 - MIN_SCALE) * (1 - MIN_ALPHA));
+
+                } else { // (1,+Infinity]
+                    // This page is way off-screen to the right.
+                    view.setAlpha(0f);
+                }
+
+            }
+        };
+
+        viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+            public int moveTo = 0;
+
+            @Override
+            public void onPageSelected(int position) {
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+                if (arg1 > 0) {
+                    int tmp1 = 0;
+                    int tmp2 = 0;
+                    moveTo = (moveTo == 0) && (arg2 > 1000) ? 2 : 1;
+
+                    if (moveTo == 1) {
+                        tmp1 = (int) (50 * arg1);
+                        tmp2 = -1 * (int) (50 * (arg1));
+                    } else {
+                        tmp1 = -1 * (int) (50 * arg1);
+                        tmp2 = (int) (50 * (arg1));
+                    }
+
+                    RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                            RelativeLayout.LayoutParams.WRAP_CONTENT,
+                            RelativeLayout.LayoutParams.WRAP_CONTENT
+                    );
+
+                    int margin = convertDpToPixel(arg0 * 50, getApplicationContext());
+
+                    params.setMargins(margin, 0, 0, 0);
+                    params.addRule(RelativeLayout.CENTER_VERTICAL);
+                    dots[0].setLayoutParams(params);
+                    if (moveTo == 1) {
+                        dots[0].setTranslationX(convertDpToPixel(tmp1, getApplicationContext()));
+                        dots[arg0 + 1].setTranslationX(convertDpToPixel(tmp2, getApplicationContext()));
+                    } else {
+                        dots[arg0 + 1].setTranslationX(convertDpToPixel(tmp1, getApplicationContext()));
+                        dots[0].setTranslationX(convertDpToPixel(tmp2, getApplicationContext()));
+                    }
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+                if (arg0 == 0) {
+                    moveTo = 0;
+                }
+            }
+        };
 
         myViewPagerAdapter = new MyViewPagerAdapter();
         viewPager.setAdapter(myViewPagerAdapter);
@@ -210,7 +204,6 @@ public class BoardingActivity extends AppCompatActivity {
 
     public void dotSetup() {
         dots = new TextView[layouts.length];
-        Log.d("dotSetup", "dotSetup: " + dots.length);
         dotsLayout.removeAllViews();
         for (int i = 0; i < dots.length; i++) {
             dots[i] = new TextView(this);
@@ -223,7 +216,7 @@ public class BoardingActivity extends AppCompatActivity {
                     RelativeLayout.LayoutParams.WRAP_CONTENT
             );
 
-            int margin = convertDpToPixel(i * 25, getApplicationContext());
+            int margin = convertDpToPixel(i * 50, getApplicationContext());
 
             params.setMargins(margin, 0, 0, 0);
             params.addRule(RelativeLayout.CENTER_VERTICAL);
@@ -301,8 +294,6 @@ public class BoardingActivity extends AppCompatActivity {
         public void destroyItem(ViewGroup container, int position, Object object) {
             View view = (View) object;
             container.removeView(view);
-
-//            test commit
         }
     }
 }
